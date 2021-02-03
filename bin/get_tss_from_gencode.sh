7fed7515ebe0a3fde9e842c0ee81a39f4413b457
Xi "Alexander" Fu <fuxialexander@users.noreply.github.com>
2021-02-02 21:25:43 -0500

    Update get_tss_from_gencode.sh

--- bin/get_tss_from_gencode.sh
+++ bin/get_tss_from_gencode.sh
@@ -1,3 +1,3 @@
 wget -O gencode.gtf.gz ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/GRCh37_mapping/gencode.v34lift37.basic.annotation.gtf.gz
 
-python gencode.gtf.gz ../test_input/regions/promoter.bed 1000
+python get_tss_from_gencode.py gencode.gtf.gz ../test_input/regions/promoter.bed 1000
